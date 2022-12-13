7101c15e1ba9362c72aba4a0571cf972c7d940ba
Sarthak Agarwal <agarwalsarthak456@gmail.com>
2022-12-13 11:18:29 +0530

    Added react for frontend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const app = express()
+
+
+const server = require('http').createServer(app)
+
+const io = require('socket.io')(server)
+
+
+io.on("connection", (socket) => {
+    console.log(socket);
+    console.log("Socket is active noww");
+
+
+    socket.on("chat", (payLoad) => {
+        console.log("Thiss is payload", payLoad);
+        io.emit("chat", payLoad)
+    })
+
+})
+
+server.listen(4000, () => {
+    console.log("Successfully Connected Server on port 4000");
+})
