8ad705c1240282688c7048c24e1b72c65898d83a
Sarthak Agarwal <agarwalsarthak456@gmail.com>
2022-12-13 11:54:43 +0530

    Completed a very simple chat App

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,11 @@ const app = express()
 
 const server = require('http').createServer(app)
 
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+    }
+})
 
 
 io.on("connection", (socket) => {
--- frontend/chatty/src/App.js
+++ frontend/chatty/src/App.js
@@ -1,22 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react'
+
+import io from 'socket.io-client'
+import { nanoid } from 'nanoid'
+
+
+const socket = io.connect('http://localhost:4000')
+
+const username = nanoid(4)
+
+
 function App() {
+
+
+  const [message, setMessage] = useState('')
+  const [chat, setChat] = useState([])
+
+  const sendChat = (e) => {
+    e.preventDefault()
+    socket.emit('chat', { message, username })
+    setMessage('')
+  }
+
+  useEffect(() => {
+    socket.on("chat", (payLoad) => {
+      setChat([...chat, payLoad])
+    })
+  })
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Chatty</h1>
+
+        {
+          chat.map((payLoad, index) => {
+            return (
+              <p key={index}>{payLoad.message} : <span> id : {payLoad.username}</span></p>
+
+            )
+          })}
+
+
+
+
+        <form onSubmit={sendChat}>
+          <input type="text"
+            name="chat"
+            placeholder='send text'
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value)
+            }}
+          />
+          <button type="submit">Send</button>
+        </form>
       </header>
     </div>
   );
